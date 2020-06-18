9df4ab856d95f9ca5a34bc2208137ae83c5a5944
veermos <mos3d.box@gmail.com>
2020-06-18 16:51:41 +0200

    edit static3

--- smart_mfis/settings.py
+++ smart_mfis/settings.py
@@ -131,7 +131,7 @@ USE_THOUSAND_SEPARATOR = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
